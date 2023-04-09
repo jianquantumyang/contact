dddabb8b2c142e36914618db3b235d2984fca69b
obozhayu <129060414+obojau@users.noreply.github.com>
2023-04-09 09:29:22 +0600

    Update settings.py

--- mektep/mektep/settings.py
+++ mektep/mektep/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9!w36t(mn865vj*f4u-luzd%c&1khti!%krx+*#loyg9f%zb52'
+SECRET_KEY = 'THISKEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
